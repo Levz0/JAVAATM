dfc1f6fce0125f45791c19d08c9c68d609ce4d92
Levz0 <levz55530@gmail.com>
2024-05-20 22:28:55 +0400

    Добавление платежной карты PlayCard

---
+++ src/PayCard.java
@@ -0,0 +1,34 @@
+import java.text.DecimalFormat;
+import java.util.Date;
+
+public class PayCard {
+    private String Number;
+    private String Owner;
+    private double Balance;
+    private Date DateGet;
+    private boolean isBlocked;
+
+    public PayCard(String Number, String Owner, double Balance, Date DateGet){
+        this.Number = Number;
+        this.Owner = Owner;
+        this.Balance = Balance;
+        this.DateGet = DateGet;
+    }
+
+    public PayCard(String Number, String Owner){
+        this.Number = Number;
+        this.Owner = Owner;
+        this.Balance = 0;
+        this.DateGet = new Date();
+    }
+
+
+    public void PutCard(ATM atm){
+
+    }
+
+
+
+
+
+}
